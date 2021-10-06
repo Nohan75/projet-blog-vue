1eb04adf73e0062317817606c292c46c4b619090
Nohan75 <nohanmarielouise@gmail.com>
2021-10-06 13:36:19 +0200

    router installé

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,51 +2,21 @@
     <div class="tabs">
         <h1>Blog</h1>
 
-        <router-link to="/Home">Home</router-link>
-        <router-link to="/About">About US</router-link>
-        <router-link to="/New">Nouveau Post</router-link>
+        <router-link to="/Home">Home</router-link> |
+        <router-link to="/About">About US</router-link> |
+        <router-link to="/New">Admin</router-link>
+
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-import TabsMenu from './TabsMenu.vue'
-import AboutUsVue from './AboutUs.vue'
-import AdminVue from './Admin.vue'
-import ListPostVue from './ListPost.vue'
 
  
 export default {
     name: 'Blog',
 
     components: {
-        TabsMenu,
-    },
-
-    data() {
-        return {
-            lastIndex: 0,
-            links: [
-                {
-                    title: 'Admin',
-                    compo: AdminVue
-                },
-                {
-                    title: 'List Post',
-                    compo: ListPostVue
-                },
-                {
-                    title: 'About US',
-                    compo: AboutUsVue
-                },
-            ]
-        }
-    },
-
-    methods: {
-        changeTab(index) {
-            console.log('change tab', index)
-            this.lastIndex = index
-        }
     },
 
 

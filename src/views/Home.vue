24f503cff00b5d177454cc22e87a1857627e377b
Nohan75 <nohanmarielouise@gmail.com>
2021-10-06 15:32:23 +0200

    affichage liste de post bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container-fluid">
-      <div class="card" style="width: 500px" v-for="(post, index) in posts" :key="index">
+  <div class="container-fluid post-container">
+      <div class="card m-4 p-2" style="width: 70%" v-for="(post, index) in posts" :key="index">
           <h3 class="card-title">  {{ post.titre }} </h3>
           <p class="card-text">  {{ post.intro }} </p>
           <p>  {{ post.date.getDate() }}/{{ post.date.getMonth() }}/{{ post.date.getYear() }} </p>
@@ -52,3 +52,10 @@ export default {
   },
 }
 </script>
+<style>
+  .post-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>

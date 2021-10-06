4b42fedef4da0da283844e1fa953b3bf3fbe5d77
Nohan75 <nohanmarielouise@gmail.com>
2021-10-06 14:25:12 +0200

    ajout du router au projet

--- src/main.js
+++ src/main.js
@@ -3,27 +3,17 @@ import App from './App.vue';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import VueRouter from 'vue-router';
-import AboutUs from './components/AboutUs.vue';
-import Admin from './components/Admin.vue';
-import ListPost from './components/ListPost.vue';
+import router from './router'
 
 Vue.use(BootstrapVue);
-Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 
-const routes = [
-  { path: '/', name: "Home", component: ListPost},
-  { path: '/About', name: 'About', component: AboutUs},
-  { path: '/New', name: 'New', component: Admin}
-]
 
-const router = new VueRouter({
-  routes
-})
+
+
 
 new Vue({
   router,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ListPost />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListPost from '@/components/ListPost.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ListPost
+  }
+}
+</script>

6c6420f99b803d69d72cfdeabbd881a3be51265c
Nohan75 <nohanmarielouise@gmail.com>
2021-10-06 14:33:32 +0200

    ajout de vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import router from './router'
+import store from './store'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
@@ -15,5 +16,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
